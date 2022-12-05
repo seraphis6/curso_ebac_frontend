b507fab75538d6072d204647b9c642daa7952c2c
~André Barbosa <73763233+seraphis6@users.noreply.github.com>
2022-12-05 11:25:14 -0300

    commit arquivos do exercicio

---
+++ main.js
@@ -0,0 +1,20 @@
+const form  = document.getElementById('form-deposito');
+
+
+let a = document.getElementById('campoA');
+
+let b = document.getElementById('campoB');
+
+a,b = parseInt('a,b');
+
+function validaçao(a,b){
+
+  return (b > a);
+
+}
+
+alert ("valido" +(validaçao));
+  
+
+
+
