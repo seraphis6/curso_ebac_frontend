c27a8b86790ad3c48f8361f9a85d94a5f42223c4
~André Barbosa <73763233+seraphis6@users.noreply.github.com>
2022-12-10 12:54:10 -0300

    update main.js

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
 const form  = document.getElementById('form-deposito');
 
 
-let a = document.getElementById('campoA');
+let valorA = document.getElementById('campoA')
 
-let b = document.getElementById('campoB');
+let valorB = document.getElementById('campoB')
 
-a,b = parseInt('a,b');
+valorA,valorB = parseInt
 
-function validaçao(a,b){
+function validar(valorA,valorB){
+  if (validar(parseInt(valorA.value), parseInt(valorB.value))){
+    let = parseInt(valorB.value) > parseInt(valorA.value);
+  }}
 
-  return (b > a);
 
-}
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
 
-alert ("valido" +(validaçao));
+  const valorA = document.getElementById('campoA')
+  const valorB = document.getElementById('campoB')
   
+    if (parseInt(valorB.value) > parseInt(valorA.value)){
+    alert("valido");
+    }
+    else !(parseInt(valorB.value) > parseInt(valorA.value));{
+      alert("Não é válido");
+    }
 
+})
 
-
+console.log(form);
